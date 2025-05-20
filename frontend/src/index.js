135fd475f8705fa050fffa78ba94e1d3ff5da4f3
David Jarrin <dmjarrin@gmail.com>
2025-05-19 21:37:31 -0400

    testing env variables

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+console.log("User Pool ID:", process.env.REACT_APP_COGNITO_USER_POOL_ID);
+console.log("Client ID:", process.env.REACT_APP_COGNITO_CLIENT_ID);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
